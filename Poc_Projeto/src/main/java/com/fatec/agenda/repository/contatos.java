6c2f0837b61f835010b0949af9773b0e5e50d1fb
Tais Nissizaki <tais.nissizaki@gmail.com>
2020-02-27 00:46:04 -0300

    feat: integração do Spring MVC com Sping Data JPA

--- Poc_Projeto/src/main/java/com/fatec/agenda/repository/contatos.java
+++ Poc_Projeto/src/main/java/com/fatec/agenda/repository/contatos.java
@@ -2,7 +2,7 @@ package com.fatec.agenda.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.fatec.agenda.model.Contato;
 
-public interface Contato extends JpaRepository <Contato, int>
+public interface Contatos extends JpaRepository <Contato, Long>
 {
     
 }
